9b8b0e5c1f7d93610cdc20995666cb687b0e9809
fourat <fourattoumi296@gmail.com>
2024-09-01 14:47:49 +0100

    first commit

---
+++ backend/controllers/dasboard/categoryController.js
@@ -0,0 +1,61 @@
+const formidable = require("formidable")
+const { responseReturn } = require("../../utiles/response")
+const cloudinary = require('cloudinary').v2
+const categoryModel = require('../../models/categoryModel')
+
+class categoryController{
+
+    add_category = async (req, res) => {
+        const form = formidable()
+        form.parse(req, async(err,fields,files)=>{
+            if (err) {
+                responseReturn(res, 404,{ error : 'something went wrong'})
+            } else {
+                let {name} = fields
+                let {image} = files
+                name = name.trim()
+                const slug = name.split(' ').join('-')
+
+                cloudinary.config({
+                    cloud_name: process.env.cloud_name,
+                    api_key: process.env.api_key,
+                    api_secret: process.env.api_secret,
+                    secure: true
+                })
+
+               try {
+
+                const result = await cloudinary.uploader.upload(image.filepath, { folder: 'categorys'})
+
+                if (result) {
+                    const category = await categoryModel.create({
+                        name,
+                        slug,
+                        image: result.url
+                    })
+                    responseReturn(res, 201,{ category,message : 'Category Added Successfully'})
+                    
+                } else {
+                    responseReturn(res, 404,{ error : 'Image Upload File'})
+                }
+                
+               } catch (error) {
+                responseReturn(res, 500,{ error : 'Internal Server Error'})
+               }
+ 
+
+
+            }
+             
+        })
+    }
+
+
+    get_category = async (req, res) => {
+        console.log(req.query)
+    }
+
+}
+ 
+
+module.exports = new categoryController()
---
+++ dashboard/src/store/rootReducers.js
@@ -0,0 +1,8 @@
+import authReducer from "./Reducers/authReducer";
+import categoryReducer from "./Reducers/categoryReducer";
+
+const rootReducer = {
+    auth: authReducer,
+    category: categoryReducer
+}
+export default rootReducer;
