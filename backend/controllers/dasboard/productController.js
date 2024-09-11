557ab2eb697edadda1565fc3b9a79f5b775e547c
Toumi Fourat <fourattoumi021@gmail.com>
2024-09-11 20:58:29 +0100

    Add Product

--- backend/controllers/dasboard/productController.js
+++ backend/controllers/dasboard/productController.js
@@ -8,27 +8,23 @@ class productController{
     add_product = async(req,res) => {
         const {id} = req;
         const form = formidable({ multiples: true })
-
         form.parse(req, async(err, field, files) => {
             let {name, category,description, stock,price, discount,shopName,brand} = field;
             const {images} = files;
             name = name.trim()
             const slug = name.split(' ').join('-')
-
             cloudinary.config({
                 cloud_name: process.env.cloud_name,
                 api_key: process.env.api_key,
                 api_secret: process.env.api_secret,
                 secure: true
             })
-
             try {
                 let allImageUrl = [];
                 for (let i = 0; i < images.length; i++) {
                     const result = await cloudinary.uploader.upload(images[i].filepath, {folder: 'products'});
                     allImageUrl = [...allImageUrl, result.url] 
                 }
-
                 await productModel.create({
                     sellerId: id,
                     name,
@@ -41,26 +37,17 @@ class productController{
                     discount: parseInt(discount),
                     images: allImageUrl,
                     brand: brand.trim()  
-
                 })
                 responseReturn(res, 201,{ message : 'Product Added Successfully'})
-
-
-
-
-
+                
             } catch (error) {
                 responseReturn(res, 500,{ error : error.message})
             }
-
-
-
-
-
+ 
         })
-
+         
     }
-    
+
     /// end method 
 
     products_get = async (req,res) => {
