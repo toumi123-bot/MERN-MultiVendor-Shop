c59f8c3c1500e687df0c78b2fb9548ff0ef528a4
Toumi Fourat <fourattoumi021@gmail.com>
2024-09-18 17:28:30 +0100

    Feature Product Design Setup

--- frontend/src/components/Categorys.jsx
+++ frontend/src/components/Categorys.jsx
@@ -15,70 +15,66 @@ const Categorys = () => {
     ]
     const responsive = {
         superLargeDesktop: {
-            breakpoint: { max: 4000, min: 3000},
+            breakpoint: { max: 4000, min: 3000 },
             items: 6
         },
         desktop: {
-            breakpoint: { max: 3000, min: 1024},
+            breakpoint: { max: 3000, min: 1024 },
             items: 6
         },
         tablet: {
-            breakpoint: { max: 1024, min: 464},
+            breakpoint: { max: 1024, min: 464 },
             items: 4
         },
         mdtablet: {
-            breakpoint: { max: 991, min: 464},
+            breakpoint: { max: 991, min: 464 },
             items: 4
         },
         mobile: {
-            breakpoint: { max: 464, min: 0},
+            breakpoint: { max: 464, min: 0 },
             items: 3
         },
         smmobile: {
-            breakpoint: { max: 640, min: 0},
+            breakpoint: { max: 640, min: 0 },
             items: 2
         },
         xsmobile: {
-            breakpoint: { max: 440, min: 0},
+            breakpoint: { max: 440, min: 0 },
             items: 1
-        }
-    
-
-
-    
+        },
     }
-    return (
-        <div>
-            <div className='w-[87%] mx-auto relative'>
-                
-                                <Carousel
-                                autoPlay={true}
-                                infinite={true}
-                                arrows={true}
-                                responsive={responsive}
-                                transitionDuration={500}
-                                
-                                >
-                                    
-                                   {
-                                    categorys.map((c,i)=> <Link className='h-[185px]
-                                    border block' key={i} to='#'>
-                                        <div className='w-full h-full relative p-3'>
-                                            <img src={`/images/products/${i+1}.webp`} alt=''/>
-                                            <div className='absolute bottom-6 w-full mx-auto
-                                            font-bold left-0 flex justify-center items-center'>
-                                                <span className='py-[2px] px-6 bg-[#3330305d]
-                                                text-white'>{c}</span>
 
-                                            </div>
-                                        </div>                                    
-                                    </Link>)
-                                   }
-                                </Carousel>
+    return (
+        <div className='w-[87%] mx-auto relative'>
+
+            <div className='w-full'>
+            <div className='text-center flex justify-center items-center flex-col text-3xl text-slate-600 font-bold relative pb-[35px]'>
+                <h2>Top Category </h2>
+                <div className='w-[100px] h-[2px] bg-[#059473] mt-4'></div>
+            </div>
             </div>
 
-        </div>
+                <Carousel
+                    autoPlay={true}
+                    infinite={true}
+                    arrows={true} 
+                    responsive={responsive}
+                    transitionDuration={500}
+                >
+                {
+                    categorys.map((c, i) => <Link className='h-[185px] border block' key={i} to='#'>
+                        <div className='w-full h-full relative p-3'>
+                        <img src={`images/products/${i+1}.webp`} alt="" />
+                        <div className='absolute bottom-6 w-full mx-auto font-bold left-0 flex justify-center items-center'>
+                         <span className='py-[2px] px-6 bg-[#3330305d] text-white'>{c}</span>
+                        </div>
+                        </div>
+                        
+                    </Link> )
+                }
+                </Carousel>        
+         </div>
+             
     );
 };
-
 export default Categorys;
---
+++ frontend/src/components/products/FeatureProducts.jsx
@@ -0,0 +1,62 @@
+import React from 'react';
+import { FaEye } from "react-icons/fa";
+import { FaRegHeart } from "react-icons/fa6";
+import { FiShoppingCart } from "react-icons/fi";
+import Rating from '../Rating';
+
+
+const FeatureProducts = () => {
+    return (
+        <div className='w-[85%] flex flex-wrap mx-auto'>
+            <div className='w-full'>
+            <div className='text-center flex justify-center items-center flex-col text-4xl text-slate-600 font-bold relative pb-[45px]'>
+                <h2>Feature Products </h2>
+                <div className='w-[100px] h-[2px] bg-[#059473] mt-4'></div>
+            </div>
+            </div>
+
+        <div className='w-full grid grid-cols-4 md-lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 gap-6'>
+    {
+        [1,2,3,4,5,6].map((p,i) => <div key={i} className='border group transition-all duration-500 hover:shadow-md hover:-mt-3'>
+            <div className='relative overflow-hidden'>
+
+        <div className='flex justify-center items-center absolute text-white w-[38px] h-[38px] rounded-full bg-red-500 font-semibold text-xs left-2 top-2'>8% </div> 
+
+        <img className='sm:w-full w-full h-[240px]' src={`http://localhost:3000/images/products/${i+1}.webp`} alt="" />  
+
+<ul className='flex transition-all duration-700 -bottom-10 justify-center items-center gap-2 absolute w-full group-hover:bottom-3'>
+    <li className='w-[38px] h-[38px] cursor-pointer bg-white flex justify-center items-center rounded-full hover:bg-[#059473] hover:text-white hover:rotate-[720deg] transition-all'>
+    <FaRegHeart />
+    </li>
+    <li className='w-[38px] h-[38px] cursor-pointer bg-white flex justify-center items-center rounded-full hover:bg-[#059473] hover:text-white hover:rotate-[720deg] transition-all'>
+    <FaEye />
+    </li>
+    <li className='w-[38px] h-[38px] cursor-pointer bg-white flex justify-center items-center rounded-full hover:bg-[#059473] hover:text-white hover:rotate-[720deg] transition-all'>
+    <FiShoppingCart  />
+    </li>
+</ul>    
+    </div>
+
+<div className='py-3 text-slate-600 px-2'>
+    <h2 className='font-bold'>Product Name </h2>
+    <div className='flex justify-start items-center gap-3'>
+        <span className='text-md font-semibold'>656 TND</span>     
+        <div className='flex'>
+                    <Rating ratings={4.5} />
+                </div>   
+
+
+            </div>
+            </div>    
+        </div>
+        )
+    }
+
+        </div>
+
+
+        </div>
+    );
+};
+
+export default FeatureProducts;
