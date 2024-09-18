ec8aa2eadea338a1c058e082e12b588ca599ebf2
Toumi Fourat <fourattoumi021@gmail.com>
2024-09-18 17:37:47 +0100

    Latest Products Design Setup

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -3,6 +3,7 @@ import Header from '../components/Header';
 import Banner from '../components/Banner';
 import Categorys from '../components/Categorys';
 import FeatureProducts from '../components/products/FeatureProducts';
+import Products from '../components/products/Products';
 
 const Home = () => {
     return (
@@ -12,6 +13,25 @@ const Home = () => {
             <Categorys/>
             <div className='py-[45px]'>
             <FeatureProducts/>
+            </div>
+            
+            <div className='py-10'>
+                <div className='w-[85%] flex flex-wrap mx-auto'>
+                    <div className='grid w-full grid-cols-3 md-lg:grid-cols-2 md:grid-cols-1 gap-7'>
+            <div className='overflow-hidden'>
+            <Products title='Latest Product' />
+            </div>
+
+            <div className='overflow-hidden'>
+            <Products title='Top Rated Product'/>
+            </div>
+
+            <div className='overflow-hidden'>
+            <Products title='Discount Product'/>
+            </div>
+
+                    </div> 
+                </div> 
             </div>
         </div>
     );
