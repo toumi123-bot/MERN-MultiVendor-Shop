70ccd9655a0d870ceee936836066038749731ded
Toumi Fourat <fourattoumi021@gmail.com>
2024-09-21 15:44:08 +0100

    Product Query for Category

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -11,6 +11,8 @@ import Login from './pages/Login';
 import { useEffect } from 'react';
 import { get_category } from './store/reducers/homeReducer';
 import { useDispatch } from 'react-redux';
+import CategoryShop from './pages/CategoryShop';
+
 
 function App() {
 
@@ -31,7 +33,9 @@ function App() {
       <Route path='/shops' element={<Shops/>} />
       <Route path='/card' element={<Card/>} />
       <Route path='/shipping' element={<Shipping/>} />
+      <Route path='/products?' element={<CategoryShop/>} />
       <Route path='/product/details/:slug' element={<Details/>} />
+
     </Routes>
     </BrowserRouter>
   );
