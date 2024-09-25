2333cc52dc3dd55334d7737741a849e0fbdff73d
Toumi Fourat <fourattoumi021@gmail.com>
2024-09-25 16:25:31 +0100

    Customer Order Dashboard Design

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -17,6 +17,8 @@ import Payment from './pages/Payment';
 import Dashboard from './pages/Dashboard';
 import ProtectUser from './utils/ProtectUser';
 import Index from './components/dashboard/Index';
+import Orders from './components/dashboard/Orders';
+
 function App() {
 
   const dispatch = useDispatch()
@@ -41,8 +43,9 @@ function App() {
       <Route path='/products/search?' element={<SearchProducts/>} />
       <Route path='/payment' element={<Payment/>} />
       <Route path='/dashboard' element={<ProtectUser/>} >
-      <Route path='' element={<Dashboard/>} >
+      <Route path='' element={<Dashboard/>} >        
       <Route path='' element={<Index/>} />
+      <Route path='my-orders' element={<Orders/>} /> 
       </Route> 
       </Route>
     </Routes>
