3a7b1517bcd5832a4ce6238c557876f015f0071a
Toumi Fourat <fourattoumi021@gmail.com>
2024-09-28 16:43:13 +0100

    Customer Chat Page Design

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -21,6 +21,8 @@ import Orders from './components/dashboard/Orders';
 import ChangePassword from './components/dashboard/ChangePassword';
 import Wishlist from './components/dashboard/Wishlist';
 import OrderDetails from './components/dashboard/OrderDetails';
+import Chat from './components/dashboard/Chat';
+
 function App() {
 
   const dispatch = useDispatch()
@@ -51,6 +53,8 @@ function App() {
       <Route path='change-password' element={<ChangePassword/>} />
       <Route path='my-wishlist' element={<Wishlist/>} /> 
       <Route path='order/details/:orderId' element={<OrderDetails/>} /> 
+      <Route path='chat' element={<Chat/>} /> 
+      <Route path='chat/:sellerId' element={<Chat/>} /> 
       </Route> 
       </Route>
     </Routes>
