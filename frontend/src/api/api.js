885daf4dcea57d64df4e65260aeb64cb70263872
ToumiFourat <fourattoumi021@gmail.com>
2025-07-15 05:48:41 +0100

    Deployy

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,8 +1,14 @@
 import axios from "axios";
-const local = 'http://localhost:5000'
-const production = ''
-const api = axios.create({
-    baseURL : `${local}/api`
-})
 
-export default api
+const local = "http://localhost:5000";
+const production =
+  "https://bimastore-backend-f9amc7fbbfhjghar.francecentral-01.azurewebsites.net";
+
+const api = axios.create({
+  baseURL:
+    process.env.NODE_ENV === "production"
+      ? `${production}/api`
+      : `${local}/api`,
+});
+
+export default api;
